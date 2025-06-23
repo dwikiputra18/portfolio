b4dfb4e273123f8afb2c892bed5d6e12cc55bf8f
dwikiputra18 <dwikiputra678@gmail.com>
2025-06-23 14:18:46 +0700

    add meta tag

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://portfolio-dwk.vercel.app', // Ganti dengan domain kamu
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
